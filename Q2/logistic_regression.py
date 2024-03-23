72dd93c2b6de8d268e35f805b99d6a13aeec2070
Simurgan <uyduranomersukru@gmail.com>
2024-03-24 02:42:18 +0300

    :D

--- Q2/logistic_regression.py
+++ Q2/logistic_regression.py
@@ -40,3 +40,29 @@ class LogisticRegression:
             for d in range(self.data.shape[0]):
                 self.data[d][i] = (self.data[d][i] - min_val) / (max_val - min_val)
             
+    # Non-regularized
+    # E(w) = 1/N * sum(log(1 + exp(-y_n * w^T * x_n)))
+    # n: initially specified learning rate
+    # n_t: learning rate at the iteration t
+    # v^: direction vector
+    # v^ = -gradient(E(w)) / ||gradient(E(w))||_2
+    # n_t = n * ||gradient(E(w_t))||_2
+    # n_t * v^ = -n * gradient(E(w_t))
+                
+    # Regularized
+    # lambda: regularization parameter
+    # E(w) = 1/N * sum(log(1 + exp(-y_n * w^T * x_n))) + lambda * w^T * w
+                
+    # GD Algorithm
+    # 1. Initialize w^0
+    # 2. For t = 0, 1, 2, ...
+    #    a. Compute v^t = gradient(E(w^t))
+    #    b. w^{t+1} = w^t - n * v^t
+    #    maybe: c. If ||v^t||_2 < epsilon, then stop (copilot suggested)
+                
+    # SGD Algorithm
+    # 1. Initialize w^0
+    # 2. pick a random data point from 1 to N
+    # 3. run an iteration of GD on that data point
+    # w(t+1) = w(t) + y_n * x_n * 1 / (1 + exp(y_n * w^T * x_n))
+
